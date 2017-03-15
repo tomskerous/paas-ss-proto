d58ef51ca8d87469766e960e5edd307e0734df80
Colm Britton <colmjude@gmail.com>
2017-03-15 11:05:23 +0000

    add js required for radio buttons

---
+++ app/static/js/app.js
@@ -0,0 +1,11 @@
+;(function ($, window) {
+  "use strict"
+
+  $(function() {
+    // Use GOV.UK selection-buttons.js to set selected
+    // and focused states for block labels
+    var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']")
+    new GOVUK.SelectionButtons($blockLabels) // eslint-disable-line
+  });
+
+}).call(this, jQuery, window);
