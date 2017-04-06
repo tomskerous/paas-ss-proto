78b33928e4e196b15ad4c049feb16294bcd32ffb
Colm Britton <colmjude@gmail.com>
2017-04-06 07:45:17 +0100

    add catch all route to handle survey questions without having to define unique routes for each

--- app/blueprints/survey/views.py
+++ app/blueprints/survey/views.py
@@ -7,3 +7,14 @@ import json
 import os
 
 survey = Blueprint('survey', __name__, url_prefix='/survey')
+
+@survey.route('/', defaults={'path': ''})
+@survey.route('/<path:path>')
+def catch_all(path):
+  # need to see if it exists
+  return render_template( 'survey/' + path + '.html')
+
+@survey.route('/using-paas-for')
+def q1():
+  # need to see if it exists
+  return render_template( 'survey/using-paas-for.html')
