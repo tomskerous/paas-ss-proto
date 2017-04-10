30df9573fd58844e62eccdea9773bde9258ddb2e
Colm Britton <colmjude@gmail.com>
2017-04-10 10:12:02 +0100

    add a few of the proto kit doc pages

--- app/blueprints/docs/views.py
+++ app/blueprints/docs/views.py
@@ -8,6 +8,12 @@ import os
 
 docs = Blueprint('docs', __name__, url_prefix='/docs')
 
+@docs.route('/', defaults={'path': ''})
+@docs.route('/<path:path>')
+def catch_all(path):
+  # need to see if it exists
+  return render_template( 'docs/' + path )
+
 @docs.route('/publishing-on-paas')
 def publishing():
   return render_template('docs/publishing-on-paas.html')
